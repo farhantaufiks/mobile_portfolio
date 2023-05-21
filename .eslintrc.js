4b5b365c2e0f2fae16a0c77d0867eec9ffe8b27e
farhantaufiks <farhantaufiks@gmail.com>
2023-05-21 18:27:52 +0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    'jest/globals': true,
+  },
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    quotes: ['error', 'single'],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'react/require-default-props': ['error'],
+    'react/default-props-match-prop-types': ['error'],
+    'react/sort-prop-types': ['error'],
+    'react-native/no-inline-styles': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        'no-inline-styles': false,
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
+};
